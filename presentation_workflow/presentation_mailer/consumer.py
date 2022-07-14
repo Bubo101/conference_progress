d6c5d417f86728c6ab6de40aab3038b9ed6a316c
Joel <jcswansontamu@gmail.com>
2022-07-13 19:21:22 -0700

    Docker compose added

--- presentation_workflow/presentation_mailer/consumer.py
+++ presentation_workflow/presentation_mailer/consumer.py
@@ -1,8 +1,10 @@
 import json
 import pika
+from pika.exceptions import AMQPConnectionError
 import django
 import os
 import sys
+import time
 from django.core.mail import send_mail
 
 
@@ -10,54 +12,57 @@ sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "presentation_mailer.settings")
 django.setup()
 
+while True:
+    try:
 
-def process_approval(ch, method, properties, body):
-    info = json.loads(body)
-    to_email = info["presenter_email"]
-    from_email = "admin@conference.go"
-    subject = "Your presentation has been accepted"
-    title = info["title"]
-    name = info["presenter_name"]
-    body = f"{name}, we're happy to tell you that your presentation {title} has been accepted"
-    send_mail(
-        subject=subject,
-        message=body,
-        from_email=from_email,
-        recipient_list=[to_email],
-        fail_silently=False,
-    )
+        def process_approval(ch, method, properties, body):
+            info = json.loads(body)
+            to_email = info["presenter_email"]
+            from_email = "admin@conference.go"
+            subject = "Your presentation has been accepted"
+            title = info["title"]
+            name = info["presenter_name"]
+            body = f"{name}, we're happy to tell you that your presentation {title} has been accepted"
+            send_mail(
+                subject=subject,
+                message=body,
+                from_email=from_email,
+                recipient_list=[to_email],
+                fail_silently=False,
+            )
 
+        def process_rejection(ch, method, properties, body):
+            info = json.loads(body)
+            to_email = info["presenter_email"]
+            from_email = "admin@conference.go"
+            subject = "Your presentation has been rejected"
+            title = info["title"]
+            name = info["presenter_name"]
+            body = f"{name}, we're sorry to tell you that your presentation {title} has been rejected"
+            send_mail(
+                subject=subject,
+                message=body,
+                from_email=from_email,
+                recipient_list=[to_email],
+                fail_silently=False,
+            )
 
-def process_rejection(ch, method, properties, body):
-    info = json.loads(body)
-    to_email = info["presenter_email"]
-    from_email = "admin@conference.go"
-    subject = "Your presentation has been rejected"
-    title = info["title"]
-    name = info["presenter_name"]
-    body = f"{name}, we're sorry to tell you that your presentation {title} has been rejected"
-    send_mail(
-        subject=subject,
-        message=body,
-        from_email=from_email,
-        recipient_list=[to_email],
-        fail_silently=False,
-    )
-
-
-parameters = pika.ConnectionParameters(host="rabbitmq")
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-channel.queue_declare(queue="presentation_approvals")
-channel.basic_consume(
-    queue="presentation_approvals",
-    on_message_callback=process_approval,
-    auto_ack=True,
-)
-channel.queue_declare(queue="presentation_rejections")
-channel.basic_consume(
-    queue="presentation_rejections",
-    on_message_callback=process_rejection,
-    auto_ack=True,
-)
-channel.start_consuming()
+        parameters = pika.ConnectionParameters(host="rabbitmq")
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+        channel.queue_declare(queue="presentation_approvals")
+        channel.basic_consume(
+            queue="presentation_approvals",
+            on_message_callback=process_approval,
+            auto_ack=True,
+        )
+        channel.queue_declare(queue="presentation_rejections")
+        channel.basic_consume(
+            queue="presentation_rejections",
+            on_message_callback=process_rejection,
+            auto_ack=True,
+        )
+        channel.start_consuming()
+    except AMQPConnectionError:
+        print("Could not connect to RabbitMQ")
+        time.sleep(2.0)
